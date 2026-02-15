b0c78ed16199a14db543aa80a3e483d69693eb44
Naveed <46922100+nedanwr@users.noreply.github.com>
2026-02-15 13:04:05 +0800

    chore: reorder imports in `main.tsx` for improved organization and consistency

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import App from "./App.tsx";
 import { ThemeProvider } from "~/components/theme-provider";
+import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
